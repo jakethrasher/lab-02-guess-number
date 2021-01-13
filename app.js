fe35a6e302f3ff958a8edf70d4892eedaf05d34b
Jake Thrasher <mjakethrasher@gmail.com>
2021-01-12 17:10:53 -0800

    add utils file

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 // import functions and grab DOM elements
+import { compareNumbers } from './utils.js';
+
 const guessAssessment = document.getElementById('guess-assess');
 const remainingGuessesSpan = document.getElementById('guesses-remaining');
 const userInput = document.getElementById('user-input');
 const resultDisplay = document.getElementById('results-display');
 const submitButton = document.getElementById('button');
+
 
 // initialize state
 let guessesRemaining = 4;
@@ -13,27 +16,55 @@ let secretNum = Math.ceil(Math.random() * 20);
 submitButton.addEventListener('click', () => {
     guessesRemaining--;
     remainingGuessesSpan.textContent = guessesRemaining;
+    
+    
 
     let userGuess = (Number(userInput.value));
-    if (userGuess < secretNum) {
-        guessAssessment.textContent = 'You guessed too low!';
-    } else if (userGuess > secretNum) {
+    let answer = compareNumbers(userGuess, secretNum);
+    if (answer === 1) {
         guessAssessment.textContent = 'You guessed too high!';
-    } else if (userGuess === secretNum) {
+    } else if (answer === -1) {
+        guessAssessment.textContent = 'You guessed too low!';
+    } else if (answer === 0) {
         guessAssessment.textContent = 'You\'re Lucky As Hell';
         userInput.style.display = 'none';
         submitButton.style.display = 'none';
         resultDisplay.textContent = 'You Won!';
     }
-
-    if (guessesRemaining === 0) {
+    if (guessesRemaining === 0 && answer !== 0) {
         userInput.style.display = 'none';
         submitButton.style.display = 'none';
         resultDisplay.textContent = 'You Lost!';
         guessAssessment.textContent = 'Sorry';
     }
+
+    console.log(secretNum);
+
+    
 });
 
+
+
+
+
+
+
+
+
+
+
+// if (userGuess < secretNum) {
+    //     guessAssessment.textContent = 'You guessed too low!';
+    // } else if (userGuess > secretNum) {
+    //     guessAssessment.textContent = 'You guessed too high!';
+    // } else if (userGuess === secretNum) {
+    //     guessAssessment.textContent = 'You\'re Lucky As Hell';
+    //     userInput.style.display = 'none';
+    //     submitButton.style.display = 'none';
+    //     resultDisplay.textContent = 'You Won!';
+    // }
+   
+    
 
 
 
