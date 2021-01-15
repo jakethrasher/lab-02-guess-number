dfff249fd7f578c1ce92cf0d23ddbf566ec4fcf8
Cherno Badjan <badjancherno@gmail.com>
2021-01-14 19:56:08 -0800

    refactor

--- app.js
+++ app.js
@@ -1,69 +1,29 @@
 // import functions and grab DOM elements
 import { compareNumbers } from './utils.js';
-
-const guessAssessment = document.getElementById('guess-assess');
+import { tooHighFunction, tooLowFunction, winFunction, lastChanceFunction } from './results-utils.js';
+const giveFeedback = document.getElementById('give-feedback');
 const remainingGuessesSpan = document.getElementById('guesses-remaining');
 const userInput = document.getElementById('user-input');
-const resultDisplay = document.getElementById('results-display');
 const submitButton = document.getElementById('button');
 
 // initialize state
 let guessesRemaining = 4;
 let secretNum = Math.ceil(Math.random() * 20);
-
+console.log(secretNum);
 // set event listeners to update state and DOM
 submitButton.addEventListener('click', () => {
     guessesRemaining--;
-    remainingGuessesSpan.textContent = guessesRemaining;
-    
+    remainingGuessesSpan.textContent = `You have: ${guessesRemaining} guesses remaining`;
+
     let userGuess = (Number(userInput.value));
     let answer = compareNumbers(userGuess, secretNum);
-    if (answer === 1) {
-        guessAssessment.textContent = 'Hmm.. you guessed too high.. go lower..';
-    } else if (answer === -1) {
-        guessAssessment.textContent = 'It seems that you guessed too low.. go higher..';
-    } else if (answer === 0) {
-        guessAssessment.textContent = 'You\'re Lucky As Hell';
-        userInput.style.display = 'none';
-        submitButton.style.display = 'none';
-        resultDisplay.textContent = 'You Won!';
-    }
-    if (guessesRemaining === 0 && answer !== 0) {
-        userInput.style.display = 'none';
-        submitButton.style.display = 'none';
-        resultDisplay.textContent = `You lost! The right number was ${secretNum}`;
-        guessAssessment.style.display = 'none';
-    }
-        
-    console.log(secretNum);
+
+    tooHighFunction(answer, giveFeedback);
+
+    tooLowFunction(answer, giveFeedback);
+
+    winFunction(answer, giveFeedback, remainingGuessesSpan, userInput, submitButton);
+
+    lastChanceFunction(guessesRemaining, answer, userInput, submitButton, giveFeedback, remainingGuessesSpan, secretNum);
 
 });
-
-    
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-   
-    
-
-
-
-
-
-
-
-
-
-
